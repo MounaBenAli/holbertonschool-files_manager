723028d83289b7d63261a90ceab09d1887583b19
MounaBenAli <benalimouna1986@gmail.com>
2022-12-13 12:08:40 +0100

    0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (error) => {
+      console.log(`${error}`);
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.setex).bind(this.client);
+    this.detAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, val, expire) {
+    return this.setAsync(key, expire, val);
+  }
+
+  async del(key) {
+    return this.setAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
