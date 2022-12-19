40832f3cd25a26ee0e77dd3089bfdc7aec34c9b0
MounaBenAli <benalimouna1986@gmail.com>
2022-12-19 16:53:09 +0100

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import { getStatus, getStats } from '../controllers/AppController';
+import { postNew } from '../controllers/UsersController';
 
 const express = require('express');
 
 const router = express.Router();
+
 router.get('/status', getStatus);
 router.get('/stats', getStats);
+router.post('/users', postNew);
 
 module.exports = router;
